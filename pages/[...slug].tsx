931f4e5cc525d33c5a39026ed97b1cda6ecf3e6f
gabo <gabriel.gaboardi13@gmail.com>
2022-07-15 23:42:29 -0300

    added initial route logic

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -1,11 +1,27 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { prisma } from "../db/client";
 
-export const getServerSideProps = async () => {
-  const page = await prisma.page.findMany();
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const slugs = context.params!.slug!;
+  const username = slugs[0];
+  const slug = slugs[1] ? slugs[1] : username;
+  console.log(`username? ${username}`);
+
+  const page = await prisma.page.findMany({
+    where: {
+      author: {
+        username: {
+          equals: `${username}`,
+        },
+      },
+      slug: {
+        equals: `${slug}`,
+      },
+    },
+  });
   return {
     props: {
       page: JSON.stringify(page),
